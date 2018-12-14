0fd7a030dc9703bb8a9818a797591308e12a4489
wangdehu <961337146@qq.com>
2018-12-14 20:05:04 +0800

    revise

--- pro/pro3/t1.c
+++
@@ -1,5 +0,0 @@
-extern void print_string(int len, char *str);
-void choose(void)
-{
-	print_string(11, "hello world");
-}
--- pro/pro4/main.c
+++
@@ -1,6 +0,0 @@
-void print(char *msg, int len);
-
-void bootmain(void)
-{
-    print("tql elon\n", 2);
-}
